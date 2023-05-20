9458a6f7879e2dd1ecff1ed7fe0200dfe08db11d
AlekseyTilinin <alexeytilinin@yandex.ru>
2023-05-20 08:11:09 +0300

    Initial Commit

---
+++ SwiftUITestApp/SwiftUITestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestAppApp.swift
+//  SwiftUITestApp
+//
+//  Created by Aleksey on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
